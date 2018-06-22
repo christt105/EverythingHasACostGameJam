31f979eb50181353f62e09f02ba99a413fc9558b
Christian Martínez <christt105@gmail.com>
2018-06-22 19:00:56 +0200

    Player movement

---
+++ Everything Has a Cost/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+    private Vector2 mov;
+    public float speed;
+
+    private bool jump;
+    private bool grounded;
+
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h;
+        float v = 0;
+        h = Input.GetAxisRaw("Horizontal");
+        //v = Input.GetAxis("Vertical");
+
+        //rb2D.IsTouching()
+
+        if (Input.GetButton("Jump"))
+            v = 2;
+        if (v != 0)
+            print(v);
+
+        mov.Set(h, v);
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(mov * speed * Time.deltaTime);
+    }
+}
